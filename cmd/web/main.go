d4ddb79ae87c3782f3a66a5fee30e9e2db5800cb
Jeremy Lim <limjeremy29@gmail.com>
2022-05-27 17:27:17 +0800

    3.1. Managing Config Settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
 	// Use the http.NewServeMux() function to initialize a new servemux (router), then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -16,12 +22,13 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// The value returned from flag.String() function is a pointer to the flag
+	// value, not the value itself. So we need to deference it before using it.
+	// We are using Printf() to interpolate the address with the log msg.
+	// If http.ListenAndServe() returns an error we use the log.Fatal() function
+	// to log the error message and exit. Note that any error returned by
+	// http.ListenAndServe() is always non-nil.
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
