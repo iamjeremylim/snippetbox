06c65f85458e39a37e211824d9211b512662776c
Jeremy Lim <limjeremy29@gmail.com>
2022-06-02 19:03:27 +0800

    3.4. Centralized Error Handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -17,7 +17,7 @@ import (
 // the http.NotFound() function to send a 404 response to the client.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -29,15 +29,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serveError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serveError(w, err)
 	}
 }
 
@@ -45,7 +43,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -64,7 +62,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		// method to write a "Method Not Allowed" response body.
 		w.Header().Set("Allow", http.MethodPost)
 		// w.WriteHeader(405) +  w.Write([]byte("Method Not Allowed")) = line 41
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
