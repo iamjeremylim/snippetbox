c2b719f4eff0c51e51466671379e2029d3c2917f
Jeremy Lim <limjeremy29@gmail.com>
2022-07-05 15:51:56 +0800

    5.4

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -39,9 +40,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 
+	buf := new(bytes.Buffer)
+
 	// Execute the template set, passing in any dynamic data.
-	err := ts.Execute(w, td)
+	err := ts.Execute(buf, td)
 	if err != nil {
 		app.serveError(w, err)
+		return
 	}
+
+	buf.WriteTo(w)
 }
